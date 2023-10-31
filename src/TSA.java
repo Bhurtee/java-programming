48e6e08327961398a36ce7a624a3119070559bf4
OlanrewajuAkibo <olanrewaju_akibo@yahoo.com>
2023-10-31 16:58:31 +0100

    all argument constructor added

--- src/TSA.java
+++ src/TSA.java
@@ -8,6 +8,19 @@ public class TSA {
     private boolean isOld;
 
     private float accountBalance;
-    
+
+    public TSA(){
+        // Empty constructor
+    }
+
+    public TSA (String name, int age, String skinColor, boolean isOld, float accountBalance){
+        this.accountBalance = accountBalance;
+        this.name = name;
+        this.age = age;
+        this.skinColor = skinColor;
+        this.isOld = isOld;
+        
+    }
+
 
 }
