59b282ecb9457412fe8de3913ad6c27385c61727
OlanrewajuAkibo <olanrewaju_akibo@yahoo.com>
2023-11-08 11:41:09 +0100

    Toyota

---
+++ src/FreshNovember.java
@@ -0,0 +1,16 @@
+public class FreshNovember {
+    public static void main(String[] args) {
+        // name = toyota, color = blue, tyres = 4;
+        String name = "Toyota Rav4";
+        String color = "Gray";
+        int tyres = 4;
+        int speed = 220;
+        double acceleration = 92.5;
+
+        System.out.println("My car name is " + name);
+        System.out.println("The color of the car is " + color);
+        System.out.println("Its number of tyres is " + tyres);
+        System.out.println("It can reach a speed of " + speed);
+        System.out.println("And also reach in 3secs, kilometer per hour of " + acceleration);
+    }
+}
